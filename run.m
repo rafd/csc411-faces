d9da93e5d77701fb836cc430f034607a8a00d519
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-12-07 11:52:11 -0500

    tweak so can use flags to choose LDAvsPixels and SVMvsLogReg

--- run.m
+++ run.m
@@ -3,15 +3,32 @@ clear
 
 % flags
 
-num_e_vecs_to_use = 10;
-train_set_size = 2500;
+num_e_vecs_to_use = 10; % only used with LDA
+train_set_size = 2000;
 flag_load_normalized_data = 1;
 flag_normalize_images = 0;
+flag_use_lda = 0;
+flag_use_svm = 1;
 options.Display = 'none';
 
 
 t = cputime;
 
+if flag_load_normalized_data || flag_normalize_images
+    fprintf('Pre-processing:  Normalized\n');
+else
+    fprintf('Pre-processing:  None\n');
+end
+if flag_use_lda
+    fprintf('Feature Extract: 2D LDA\n');
+else
+    fprintf('Feature Extract: Pixel\n');
+end
+if flag_use_svm
+    fprintf('Learning Method: SVM\n');
+else
+    fprintf('Learning Method: LogReg\n');
+end
 
 %
 % Load TFD Dataset
@@ -21,7 +38,6 @@ t = cputime;
 %   data_train: 2925x48x48 grayscale images
 %   targets_train: 2925x7 labels
 
-fprintf('Loading Data...\n');
 if flag_load_normalized_data 
     load TFD_hw4_train_norm
 else
@@ -45,7 +61,6 @@ labels_test = targets_train(train_set_size+1:size(targets_train,1),:);
 nClasses = size(labels_train,2); % 7
 [trainInstances, imgRows, imgCols] = size(faces_train);
 testInstances = size(faces_test,1);
-nVars = imgRows * num_e_vecs_to_use;
 
 %
 % Sort Training Data
@@ -76,7 +91,7 @@ if flag_normalize_images && ~flag_load_normalized_data
 
     % rewrite images histogram equalized to reference histogram
     for i=1:size(data_train,1)
-        data_train(i,:,:) = medfilt2(histeq(reshape(data_train(i,:,:),imgRows,imgCols),ref_hist)); 
+        data_train(i,:,:) = histeq(reshape(data_train(i,:,:),imgRows,imgCols),ref_hist); 
     end
 end
 
@@ -84,73 +99,114 @@ end
 % Compute Features with 2D-LDA
 %
 
-fprintf('Computing Features...\n');
 
-% Resize into the form desired by the 2D LDA function
-for i=1:trainInstances
-    trainSample{i}=reshape(faces_train(i,:,:), imgRows, imgCols); 
+if flag_use_lda
+    fprintf('Computing Features...\n');
+
+    nVars = imgRows * num_e_vecs_to_use;
+    
+    % Resize into the form desired by the 2D LDA function
+    for i=1:trainInstances
+        trainSample{i}=reshape(faces_train(i,:,:), imgRows, imgCols); 
+    end
+
+    [vec, val] = tdfda(trainSample, nClasses, labels_train);
+
+    % Compute modified faces
+    vec = vec(:,1:num_e_vecs_to_use);
+
+    features_train = zeros(trainInstances, nVars);
+    for i=1:trainInstances
+    	features_train(i,:,:) = reshape((reshape(faces_train(i,:,:), imgRows, imgCols)*vec),nVars,1);
+    end
+
+else
+
+    features_train = zeros(trainInstances, imgCols*imgRows);
+    for i=1:trainInstances
+        features_train(i,:,:) = reshape(faces_train(i,:,:), imgRows*imgCols, 1);
+    end
+    nVars = imgRows*imgCols;
+
 end
 
-[vec, val] = tdfda(trainSample, nClasses, labels_train);
 
-% Compute modified faces
-vec = vec(:,1:num_e_vecs_to_use);
+if flag_use_svm
+    %
+    % Multi-class Linear SVM
+    %
 
-features_train = zeros(trainInstances, nVars);
-for i=1:trainInstances
-	features_train(i,:,:) = reshape((reshape(faces_train(i,:,:), imgRows, imgCols)*vec),nVars,1);
+    fprintf('Training SVM...\n')
+
+    lambda = 1e-2;
+
+    funObj = @(w)SSVMMultiLoss(w,features_train,labels_train,nClasses);
+    wLinear = minFunc(@penalizedL2,zeros(nVars*nClasses,1),options,funObj,lambda);
+    wLinear = reshape(wLinear,[nVars nClasses]);
+else
+
+    %
+    % Multinomial Logistic Regression
+    %
+
+    fprintf('Training LogReg...\n');
+
+    X = [ones(trainInstances,1) features_train];
+    funObj = @(W)SoftmaxLoss2(W,X,labels_train,nClasses);
+
+    lambda = 1e-4*ones(nVars+1,nClasses-1);
+    lambda(1,:) = 0; % Don't penalize biases
+    
+    wSoftmax = minFunc(@penalizedL2,zeros((nVars+1)*(nClasses-1),1),options,funObj,lambda(:));
+    wSoftmax = reshape(wSoftmax,[nVars+1 nClasses-1]);
+    wSoftmax = [wSoftmax zeros(nVars+1,1)];
 end
-
-
-%
-% Multi-class Linear SVM
-%
-%{
-fprintf('Compute SVM...\n')
-
-lambda = 1e-2;
-
-funObj = @(w)SSVMMultiLoss(w,features_train,labels_train,nClasses);
-wLinear = minFunc(@penalizedL2,zeros(nVars*nClasses,1),options,funObj,lambda);
-wLinear = reshape(wLinear,[nVars nClasses]);
-%}
-
-%
-% Multinomial Logistic Regression
-%
-
-X = [ones(trainInstances,1) features_train];
-funObj = @(W)SoftmaxLoss2(W,X,labels_train,nClasses);
-
-lambda = 1e-4*ones(nVars+1,nClasses-1);
-lambda(1,:) = 0; % Don't penalize biases
-fprintf('Training multinomial logistic regression model...\n');
-wSoftmax = minFunc(@penalizedL2,zeros((nVars+1)*(nClasses-1),1),options,funObj,lambda(:));
-wSoftmax = reshape(wSoftmax,[nVars+1 nClasses-1]);
-wSoftmax = [wSoftmax zeros(nVars+1,1)];
 
 %
 % Compute Error
 %
 
-fprintf('Compute Error...\n')
+if flag_use_lda
 
-% Training
-[junk yhat] = max(X*wSoftmax,[],2);
-trainErr = sum(yhat~=labels_train)/length(labels_train);
+    % Test
+    [y,junk] = find(labels_test');
+    features_test = zeros(testInstances, nVars);
+    for i=1:testInstances
+        features_test(i,:,:) = reshape((reshape(faces_test(i,:,:), imgRows, imgCols)*vec), nVars, 1);
+    end
 
-% Test
+else
 
-% Test
+    % Test
+    [y,junk] = find(labels_test');
+    features_test = zeros(testInstances, nVars);
+    for i=1:testInstances
+        features_test(i,:,:) = reshape(faces_test(i,:,:), imgRows*imgCols, 1);
+    end
 
-[y,junk] = find(labels_test');
-features_test = zeros(testInstances, nVars);
-for i=1:testInstances
-	features_test(i,:,:) = reshape((reshape(faces_test(i,:,:), imgRows, imgCols)*vec), nVars, 1);
 end
 
-[junk yhat_test] = max([ones(testInstances,1) features_test]*wSoftmax,[],2);
-testErr = sum(yhat_test~=y)/length(y);
+if flag_use_svm
+
+    % Training
+    [junk yhat] = max(features_train*wLinear,[],2);
+    trainErr = sum(labels_train~=yhat)/length(labels_train);
+     
+    % Test
+    [junk yhat_test] = max(features_test*wLinear,[],2);
+    testErr = sum(y~=yhat_test)/length(y);
+
+else
+
+    % Training
+    [junk yhat] = max(X*wSoftmax,[],2);
+    trainErr = sum(yhat~=labels_train)/length(labels_train);
+
+    [junk yhat_test] = max([ones(testInstances,1) features_test]*wSoftmax,[],2);
+    testErr = sum(yhat_test~=y)/length(y);
+
+end
+
 
 fprintf('Run Time:       %3.0f\n',cputime-t)
 fprintf('Train Error: %6.2f\n', trainErr)
