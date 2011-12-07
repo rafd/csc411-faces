39383af404d46ee7d1bbd80ebb5254f706f76dc1
Rafal Dittwald <rafal.dittwald@gmail.com>
2011-12-06 20:30:46 -0500

    svm

--- facemagic.m
+++ facemagic.m
@@ -1,14 +1,21 @@
+clc
+clear
+
 % flags
 
 flag_load_normalized_data = 1;
 flag_normalize_images = 0;
+options.Display = 'none';
 
+
+t = cputime;
 
 % Load TFD Dataset
 % provides:
 %   data_train: 2925x48x48 grayscale images
 %   targets_train: 2925x7 labels
 
+fprintf('Loading Data...\n');
 if flag_load_normalized_data 
     load TFD_hw4_train_norm
 else
@@ -18,6 +25,8 @@ end
 % 1: Normalize Histogram of Images
 
 if flag_normalize_images && ~flag_load_normalized_data
+    fprintf('Normalizing Images...%d\n',cputime-t)
+    
     % get hist of first image
     ref_hist = imhist(reshape(data_train(1,:,:),image_rows,image_cols));
 
@@ -30,17 +39,42 @@ else
 end
 
 
-[image_count, image_rows, image_cols] = size(data_train);
+% Ref Variables
+
+[nInstances, image_rows, image_cols] = size(data_train);
+nVars = 5; 
+nClasses = size(targets_train,2); % 7
 
 
 % 2: 2D-LDA
+
+fprintf('Computing Features...\n');
+
 
 
 
 
 % 3: Multi-class Linear SVM
 
+fprintf('Compute SVM... %d\n',cputime-t)
 
+X = double(data_train(:,1:nVars));
+
+% return the class # of the targets
+[y,junk] = find(targets_train');
+lambda = 1e-2;
+
+% Linear
+funObj = @(w)SSVMMultiLoss(w,X,y,nClasses);
+wLinear = minFunc(@penalizedL2,zeros(nVars*nClasses,1),options,funObj,lambda);
+
+fprintf('Reshape... %d\n',cputime-t)
+wLinear = reshape(wLinear,[nVars nClasses]);
+
+% compute error
+fprintf('Compute Error...%d\n',cputime-t)
+[junk yhat] = max(X*wLinear,[],2);
+trainErr_linear = sum(y~=yhat)/length(y);
 
 
 
